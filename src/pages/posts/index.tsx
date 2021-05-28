ea6c5f61d50d6d113855ed146e244746c8ad1e07
Abilio Azevedo <abilio10@gmail.com>
2021-05-28 13:58:37 -0300

    fix: title format

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -25,8 +25,8 @@ const Post: React.FC<PostsProps> = ({ posts }) => (
 
     <main className={styles.container}>
       <div className={styles.posts}>
-        {posts.map(post => (
-          <Link key={post.slug}href={`/posts/preview/${post.slug}`}>
+        {posts?.map(post => (
+          <Link key={post.slug} href={`/posts/preview/${post.slug}`}>
             <a>
               <time>{post.updatedAt}</time>
               <strong>{post.title}</strong>
@@ -46,22 +46,23 @@ export const getStaticProps: GetStaticProps = async () => {
     [Prismic.predicates.at('document.type', 'post')],
     { fetch: ['post.title', 'post.content'], pageSize: 100 },
   );
-
-  const posts = response.results.map(post => ({
-    slug: post.uid,
-    title: RichText.asText(post.data.title),
-    excerpt:
-      post.data.content.find(content => content.type === 'paragraph')?.text ??
-      '',
-    updatedAt: new Date(post.last_publication_date).toLocaleDateString(
-      'pt-BR',
-      {
-        day: '2-digit',
-        month: 'long',
-        year: 'numeric',
-      },
-    ),
-  }));
+  const posts = response?.results?.map(post => {
+    return {
+      slug: post?.uid,
+      title: post?.data?.title,
+      excerpt:
+        post?.data?.content?.find(content => content.type === 'paragraph')?.text ??
+        '',
+      updatedAt: new Date(post?.last_publication_date).toLocaleDateString(
+        'pt-BR',
+        {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
+        },
+      ),
+    };
+  });
 
   return { props: { posts }, revalidate: 60 * 60 }; // 1 hour;
 };
